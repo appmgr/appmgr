45bd5a174e1f1779e333490dd9a07435e10a6b82
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-02-03 22:37:36 +0100

    Update package script

--- build-package.sh
+++ build-package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Might be needed --git-pristine-tar --git-pristine-tar-commit
-gbp buildpackage --git-upstream-tag='v%(version)s' --git-upstream-branch=master --git-debian-branch=debian/sid --git-builder=debuild -S
+gbp buildpackage --git-upstream-tag='v%(version)s' --git-upstream-branch=master --git-submodules --git-debian-branch=debian/sid --git-builder=debuild -S -sa
